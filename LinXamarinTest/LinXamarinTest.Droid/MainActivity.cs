7b55e3ab2e365e1e1fc56c7711f56bfefcac9c60
Hongjuan Lin gateaux <Hongjuan.lingateaux@hotmail.com>
2017-02-26 19:25:08 +0100

    Android Push Notification with FCM, msg send from FCM console

--- LinXamarinTest/LinXamarinTest.Droid/MainActivity.cs
+++ LinXamarinTest/LinXamarinTest.Droid/MainActivity.cs
@@ -7,18 +7,71 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
 namespace LinXamarinTest.Droid
 {
 	[Activity (Label = "Lin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
+        TextView msgText;
+        const string TAG = "MainActivity";
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new LinXamarinTest.App ());
-		}
-	}
+            Log.Debug(TAG, "google app id: " + Resource.String.google_app_id);
+
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
+
+
+            SetContentView(Resource.Layout.Main);
+
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
+
+            var logTokenButton = FindViewById<Button>(Resource.Id.logTokenButton);
+            logTokenButton.Click += delegate {
+                Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+            };
+
+            //IsPlayServicesAvailable();
+
+            global::Xamarin.Forms.Forms.Init (this, bundle);
+
+            LoadApplication (new LinXamarinTest.App ());
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText.Text = "Sorry, this device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+    }
 }
 
